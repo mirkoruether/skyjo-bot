5c40064aead1b57b6ac1866fb1232a58a9bbe0e6
Mirko Rüther <mirkoruether@users.noreply.github.com>
2022-06-06 23:17:23 +0200

    more game implementation

--- game.py
+++ game.py
@@ -9,9 +9,22 @@ import typing
 import abc
 import numpy as np
 
-START_CARD_DECK = np.array(itertools.chain.from_iterable([
-    [-2] * 10,
-
+START_CARD_DECK = list(itertools.chain.from_iterable([
+    [-2] * 5,
+    [ 0] * 15,
+    [-1] * 10,
+    [ 1] * 10,
+    [ 2] * 10,
+    [ 3] * 10,
+    [ 4] * 10,
+    [ 5] * 10,
+    [ 6] * 10,
+    [ 7] * 10,
+    [ 8] * 10,
+    [ 9] * 10,
+    [10] * 10,
+    [11] * 10,
+    [12] * 10,
 ]))
 
 class IllegalGameAction(RuntimeError):
@@ -23,19 +36,18 @@ class CardStatus:
     GONE = 2
 
 class CurrentGameInfo:
-    _values = None
-    _status = None
-    _open = None
+    _values:np.ndarray = None
+    _status:np.ndarray = None
+    _open:int = None
 
     def __init__(self, values, status, topdis) -> None:
-        self._values : np.ndarray = values
-        self._status : np.ndarray = status
-        self._topdis : int = topdis
+        self._values = values
+        self._status = status
+        self._topdis = topdis
 
 class Player(abc.ABC):
-    @abc.abstractmethod
     def reveal_two(self, cgi:CurrentGameInfo) -> typing.Tuple[int, int]:
-        pass
+        return random.randint(0, 11), random.randint(0, 11)
 
     @abc.abstractmethod
     def choose_draw_from_discarded(self, cgi:CurrentGameInfo) -> bool:
@@ -45,25 +57,19 @@ class Player(abc.ABC):
     def choose_action(self, cgi:CurrentGameInfo, card:int) -> typing.Tuple[bool, int]:
         pass
 
-class RoundResult:
-    pass
-
-class GameResult:
-    pass
-
 class Game:
-    _deck = None
-    _discarded = None
-    _topdiscard = None
-    _player_card_value = None
-    _player_card_status = None
+    _deck : typing.List[int] = None
+    _discarded : typing.List[int] = None
+    _topdiscard : int = None
+    _player_card_value : np.ndarray = None
+    _player_card_status : np.ndarray = None
 
     _players : typing.List[Player] = None
 
     def __init__(self, players) -> None:
         self._players = players
 
-    def play_round(self) -> RoundResult:
+    def play_round(self) -> np.ndarray:
         self.init_next_round()
         gameinfo = self.calculate_game_info()
 
@@ -78,7 +84,7 @@ class Game:
         playeridx = gameinfo._values.sum(axis=1).argmax()
         turnno = 0
         turncnt = 500
-        game_finished = False
+        finishing_playeridx = None
 
         while turnno <= turncnt:
             gameinfo = self.calculate_game_info()
@@ -108,26 +114,37 @@ class Game:
 
             # Check if triplet is present
             # If yes, set status to "gone", value to "0" and discard
-            for i in range(4):
-                if (self._player_card_status[playeridx, 3*i:3*(i+1)] == CardStatus.REVEALED).all()\
-                  and (self._player_card_value[playeridx, 3*i:3*(i+1)] == self._player_card_value[playeridx, 3*i]).all():
-
-                    triplet_card = self._player_card_value[playeridx, 3*i]
-                    self._player_card_status[playeridx, 3*i:3*(i+1)] = CardStatus.GONE
-                    self._player_card_value[playeridx, 3*i:3*(i+1)] = 0
-                    for i in range(3):
-                        self.discard(triplet_card)
+            self.check_and_handle_triplets(playeridx)
 
             # Check if the game is finished (and was not finished before)
             # If yes the other players only get one more turn
-            if not game_finished and self.check_game_finished():
-                game_finished = True
+            if finishing_playeridx is None and self.check_game_finished():
+                finishing_playeridx = playeridx
                 turncnt = turnno + len(self._players)
 
+            turnno = turnno + 1
+            playeridx = (playeridx + 1) % len(self._players)
+        # end while 
 
+        # Conclude game
+        self._player_card_status = np.where(
+            self._player_card_status == CardStatus.HIDDEN,
+            CardStatus.REVEALED,
+            self._player_card_status,
+        )
+
+        for i in range(len(self._players)):
+            self.check_and_handle_triplets(i)
+
+        final_values = self._player_card_value.sum(axis=1)
+        if finishing_playeridx is not None:
+            if final_values[finishing_playeridx] > final_values.min(): # ToDo: It's also x2 if a second player has equal score
+                final_values[finishing_playeridx] = 2 * final_values[finishing_playeridx]
+
+        return final_values
 
     def init_next_round(self):
-        self._discarded = np.zeros(0)
+        self._discarded = []
         self._player_card_value = np.zeros((len(self._players), 12))
         self._player_card_status = np.zeros((len(self._players), 12))
 
@@ -144,6 +161,17 @@ class Game:
 
     def check_game_finished(self) -> bool:
         return (self._player_card_status != CardStatus.HIDDEN).all(axis=1).any()
+
+    def check_and_handle_triplets(self, playeridx) -> None:
+        for i in range(4):
+            if (self._player_card_status[playeridx, 3*i:3*(i+1)] == CardStatus.REVEALED).all()\
+                and (self._player_card_value[playeridx, 3*i:3*(i+1)] == self._player_card_value[playeridx, 3*i]).all():
+
+                triplet_card = self._player_card_value[playeridx, 3*i]
+                self._player_card_status[playeridx, 3*i:3*(i+1)] = CardStatus.GONE
+                self._player_card_value[playeridx, 3*i:3*(i+1)] = 0
+                for i in range(3):
+                    self.discard(triplet_card)
 
     def calculate_game_info(self) -> CurrentGameInfo:
         return CurrentGameInfo(
