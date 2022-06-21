9d7417a5b661b8d5587bb14723474ed83fd8eee7
Mirko Rüther <mirkoruether@users.noreply.github.com>
2022-06-21 18:39:41 +0200

    Rename config to neat-config.ini

--- neat_tournament.py
+++ neat_tournament.py
@@ -173,5 +173,5 @@ class NeatTournament:
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, "neat-config")
+    config_path = os.path.join(local_dir, "neat-config.ini")
     NeatTournament(NeatDuelist54).run(config_path)
